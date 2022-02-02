04c21c4ae2c61b0f2cb3897270365c347396dfaa
gagaga231 <97590675+gagaga231@users.noreply.github.com>
2022-02-02 10:36:12 +0300

    Update bot_test.py

--- bot_test.py
+++ bot_test.py
@@ -1,5 +1,5 @@
 import telebot
-bot = telebot.TeleBot("", parse_mode = None)
+bot = telebot.TeleBot("988777574:AAH8_92VYMFCtSyOc43S23aGgra_H7Vncag", parse_mode = None)
 @bot.message_handler(commands=['star', 'help'])
 def send_message(message):
     bot.reply_to(message, "дароу")
